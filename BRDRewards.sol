cd55666c89a81911db36f073302c0ac446ce6549
Walter Thornton <wjt17@hotmail.com>
2022-06-12 17:44:10 -0400

    Update BRDRewards.sol

--- BRDRewards.sol
+++ BRDRewards.sol
@@ -10,7 +10,7 @@ contract BRDRewards is ERC20, ERC20Burnable, Ownable {
 
   mapping(address => bool) controllers;
   
-  constructor() ERC20("BRDRewards", "BRDT") { }
+  constructor() ERC20("BRDRewards", "BRDR") { }
 
   function mint(address to, uint256 amount) external {
     require(controllers[msg.sender], "Only controllers can mint");
