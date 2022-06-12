57380c387c8fd0ac96858ce61d6a18c23fb0a6d1
Walter Thornton <wjt17@hotmail.com>
2022-06-12 17:38:42 -0400

    Update BRDNFTStaking.sol

--- BRDNFTStaking.sol
+++ BRDNFTStaking.sol
@@ -22,12 +22,12 @@ contract NFTStaking is Ownable, IERC721Receiver {
 
   // reference to the Block NFT contract
   Collection nft;
-  N2DRewards token;
+  BRDRewards token;
 
   // maps tokenId to stake
   mapping(uint256 => Stake) public vault; 
 
-   constructor(Collection _nft, N2DRewards _token) { 
+   constructor(Collection _nft, BRDRewards _token) { 
     nft = _nft;
     token = _token;
   }
